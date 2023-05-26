c4f053f1c514187147273873d4ae2f2f5ba63bf2
kevinmilet <kevin.milet@gmail.com>
2023-05-26 16:44:46 +0200

    ...

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -16,6 +16,7 @@ const Header = () => {
 			<nav className={`navbar ${showLinks ? 'show-nav' : ''}`}>
 				<div className='navbar-logo'>
 					<img src={Logo} alt='Logo' className='logo' />
+					<h3>Mon Congélateur</h3>
 				</div>
 				<ul className='navbar-links'>
 					{menuData.map(item => (
--- src/pages/authentified/Product/ProductTable.js
+++ src/pages/authentified/Product/ProductTable.js
@@ -6,6 +6,65 @@ import { utilsService } from '../../../services/utils.service';
 import Loader from '../../../components/Tools/Loader';
 require('dayjs/locale/fr');
 
+const columns = [
+	{
+		name: 'Nom',
+		selector: row => utilsService.capitalize(row?.name),
+		sortable: true,
+	},
+	{
+		name: 'Quantité',
+		selector: row => row?.quantity,
+		sortable: true,
+	},
+	{
+		name: "Date d'ajout",
+		selector: row => dayjs(row.adding_date).locale('fr').format('DD MMMM YYYY'),
+		sortable: true,
+	},
+	{
+		name: 'Type de produit',
+		selector: row => utilsService.capitalize(row?.ProductType?.name),
+		sortable: true,
+	},
+];
+
+const paginationComponentOptions = {
+	rowsPerPageText: 'Produits par pages',
+	rangeSeparatorText: 'sur',
+	selectAllRowsItem: true,
+	selectAllRowsItemText: 'Toutes',
+};
+
+const customStyles = {
+	table: {
+		style: {
+			color: '#212738ff',
+			borderRadius: '16px',
+		},
+	},
+	rows: {
+		style: {
+			'&:hover': {
+				backgroundColor: 'rgba(213, 86, 114, .5)',
+				cursor: 'pointer',
+			},
+		},
+	},
+	head: {
+		style: {
+			fontSize: '16px',
+			fontWeight: 800,
+			color: '#212738ff',
+		},
+	},
+	noData: {
+		style: {
+			padding: '30px',
+		},
+	},
+};
+
 const ProductTable = ({ freezerId }) => {
 	const localizedFormat = require('dayjs/plugin/localizedFormat');
 	dayjs.extend(localizedFormat);
@@ -13,66 +72,6 @@ const ProductTable = ({ freezerId }) => {
 	const [products, setProducts] = useState([]);
 	const [selectedRows, setSelectedRows] = React.useState(false);
 	const [loading, setLoading] = useState(true);
-
-	const columns = [
-		{
-			name: 'Nom',
-			selector: row => utilsService.capitalize(row?.name),
-			sortable: true,
-		},
-		{
-			name: 'Quantité',
-			selector: row => row?.quantity,
-			sortable: true,
-		},
-		{
-			name: "Date d'ajout",
-			selector: row =>
-				dayjs(row.adding_date).locale('fr').format('DD MMMM YYYY'),
-			sortable: true,
-		},
-		{
-			name: 'Type de produit',
-			selector: row => utilsService.capitalize(row?.ProductType?.name),
-			sortable: true,
-		},
-	];
-
-	const paginationComponentOptions = {
-		rowsPerPageText: 'Produits par pages',
-		rangeSeparatorText: 'sur',
-		selectAllRowsItem: true,
-		selectAllRowsItemText: 'Toutes',
-	};
-
-	const customStyles = {
-		table: {
-			style: {
-				color: '#212738ff',
-				borderRadius: '16px',
-			},
-		},
-		rows: {
-			style: {
-				'&:hover': {
-					backgroundColor: 'rgba(213, 86, 114, .5)',
-					cursor: 'pointer',
-				},
-			},
-		},
-		head: {
-			style: {
-				fontSize: '16px',
-				fontWeight: 800,
-				color: '#212738ff',
-			},
-		},
-		noData: {
-			style: {
-				padding: '30px',
-			},
-		},
-	};
 
 	const handleChange = ({ selectedRows }) => {
 		console.log('Selected Rows: ', selectedRows);
@@ -84,40 +83,39 @@ const ProductTable = ({ freezerId }) => {
 	};
 
 	useEffect(() => {
-		productService
-			.getProductsByFreezerid(freezerId)
-			.then(response => {
-				setProducts(response.data.data);
-			})
-			.catch(error => console.error(error))
-			.finally(
-				setTimeout(() => {
-					setLoading(false);
-				}, '1000')
-			);
+		if (freezerId !== undefined) {
+			productService
+				.getProductsByFreezerid(freezerId)
+				.then(response => {
+					setProducts(response.data.data);
+				})
+				.catch(error => console.error(error))
+				.finally(
+					setTimeout(() => {
+						setLoading(false);
+					}, '1000')
+				);
+		}
 	}, [freezerId]);
 
 	return (
 		<div>
-			{products.length > 0 ? (
-				<DataTable
-					columns={columns}
-					data={products}
-					selectableRows
-					customStyles={customStyles}
-					onSelectedRowsChange={handleChange}
-					pagination
-					paginationPerPage={10}
-					paginationRowsPerPageOptions={[5, 10, 15, 20]}
-					paginationComponentOptions={paginationComponentOptions}
-					onRowClicked={row => handleClick(row)}
-					noDataComponent='Votre congélateur est vide !!!'
-					progressPending={loading}
-					progressComponent={<Loader />}
-				/>
-			) : (
-				'Votre congélateur est vide !!!'
-			)}
+			{/* {products.length > 0 ? ( */}
+			<DataTable
+				columns={columns}
+				data={products}
+				selectableRows
+				customStyles={customStyles}
+				onSelectedRowsChange={handleChange}
+				pagination
+				paginationPerPage={10}
+				paginationRowsPerPageOptions={[5, 10, 15, 20]}
+				paginationComponentOptions={paginationComponentOptions}
+				onRowClicked={row => handleClick(row)}
+				noDataComponent='Votre congélateur est vide !!!'
+				progressPending={loading}
+				progressComponent={<Loader />}
+			/>
 		</div>
 	);
 };
