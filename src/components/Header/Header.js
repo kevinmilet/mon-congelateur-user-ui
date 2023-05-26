cd4a0a6b4993cbe784556d921b9ec6a0a773fac6
kevinmilet <kevin.milet@gmail.com>
2023-05-26 18:09:13 +0200

    modification menu + ajout logout

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,21 @@
 import React, { useState } from 'react';
-import menuData from '../../utils/menuData';
+import { authService } from '../../services/auth.service';
 import './header.scss';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import Logo from '../../assets/icons/congelateur.png';
 
 const Header = () => {
+	const navigate = useNavigate();
 	const [showLinks, setShowLinks] = useState(false);
 
 	const handleShowLinks = () => {
 		setShowLinks(!showLinks);
+	};
+
+	const logout = () => {
+		console.log('logout');
+		authService.logout();
+		navigate('/');
 	};
 
 	return (
@@ -18,13 +25,29 @@ const Header = () => {
 					<img src={Logo} alt='Logo' className='logo' />
 				</div>
 				<ul className='navbar-links'>
-					{menuData.map(item => (
-						<li key={item.id} className={`navbar-item slide-${item.id}`}>
-							<Link to={item.path} className='navbar-link'>
-								{item.name}
-							</Link>
-						</li>
-					))}
+					<li className='navbar-item slide-1'>
+						<Link to={'/monespace/home'} className='navbar-link'>
+							Accueil
+						</Link>
+					</li>
+					<li className='navbar-item slide-2'>
+						<Link to={'/monespace/freezers'} className='navbar-link'>
+							Mes congélateurs
+						</Link>
+					</li>
+					<li className='navbar-item slide-3'>
+						<Link to={'/monespace/products'} className='navbar-link'>
+							Mes produits
+						</Link>
+					</li>
+					<li className='navbar-item slide-4'>
+						<Link to={'/monespace/account'} className='navbar-link'>
+							Mon compte
+						</Link>
+					</li>
+					<li className='navbar-item slide-5' onClick={logout}>
+						<span className='navbar-link'>Déconnexion</span>
+					</li>
 				</ul>
 				<button className='navbar-burger' onClick={handleShowLinks}>
 					<span className='burger-bar'></span>
